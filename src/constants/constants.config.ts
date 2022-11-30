0a61b5d11420da0322bef0188b0b62ea5d32c327
LeKhai-KLB <khaileklb1111@gmail.com>
2022-12-01 06:00:54 +0700

    up

--- src/constants/constants.config.ts
+++ src/constants/constants.config.ts
@@ -1,2 +1,3 @@
-export const GRAPHQL_URI = process.env.REACT_APP_GRAPHQL_URI || "";
+export const GRAPHQL_URI =
+  "https://company-management-server-production.up.railway.app/graphql";
 // export const GRAPHQL_URI = "http://192.168.101.45:4000/graphql";
